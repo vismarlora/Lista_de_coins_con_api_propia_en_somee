b634ac3e0f964120863c9cb08aac59de9e2c92a6
vismarlora <vismar_lora@ucne.edu.do>
2022-06-25 18:10:01 -0400

    Configurando Hilt

---
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/ListaCoins.kt
@@ -0,0 +1,8 @@
+package com.example.listadecoinsconapipropiaensomee
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ListaCoins: Application() {
+}
