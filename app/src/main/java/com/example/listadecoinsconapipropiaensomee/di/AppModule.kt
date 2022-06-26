09ca5446b9bb8b34a812d9677511e483b7d18b98
vismarlora <vismar_lora@ucne.edu.do>
2022-06-25 20:35:42 -0400

    Creando la clase de tipo objeto de AppModule

---
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.example.listadecoinsconapipropiaensomee.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+}
