232a5381330ab12340e50fa3dfdd02eba1705641
vismarlora <vismar_lora@ucne.edu.do>
2022-06-25 20:36:42 -0400

    Heredando AndroidEntryPoint en el MainActivity.kt

--- app/src/main/java/com/example/listadecoinsconapipropiaensomee/MainActivity.kt
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.listadecoinsconapipropiaensomee.ui.theme.ListaDeCoinsConApiPropiaEnSomeeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
