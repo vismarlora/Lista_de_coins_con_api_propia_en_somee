f3d4d86692036f0a0a43d5f48d8e036d8cb75ffd
vismarlora <vismar_lora@ucne.edu.do>
2022-07-03 22:21:51 -0400

    Cambiando el tipo de dato de la variable valor a Double

--- app/src/main/java/com/example/listadecoinsconapipropiaensomee/data/dto/CoinDto.kt
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/data/dto/CoinDto.kt
@@ -1,8 +1,11 @@
 package com.example.listadecoinsconapipropiaensomee.data.dto
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CoinDto(
     val monedaId: Int = 0,
     val descripcion: String = "",
-    val valor: String = "",
+    val valor: Double = 0.0,
     val imageUrl: String = ""
 )
