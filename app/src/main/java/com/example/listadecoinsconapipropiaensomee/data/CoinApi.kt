4fe8d9e9744e4562eb2153f6ddf0d8a67a1f2d29
vismarlora <vismar_lora@ucne.edu.do>
2022-07-03 22:19:40 -0400

    Agregando el POST en el CoinApi.kt para poder insertar monedas en la consulta

--- app/src/main/java/com/example/listadecoinsconapipropiaensomee/data/CoinApi.kt
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/data/CoinApi.kt
@@ -1,11 +1,16 @@
 package com.example.listadecoinsconapipropiaensomee.data
 
 import com.example.listadecoinsconapipropiaensomee.data.dto.CoinDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface CoinApi {
     @GET("/Coinst")
     suspend fun getCoins(): List<CoinDto>
+
+    @POST("/Coinst")
+    suspend fun PostInser(@Body coinDto: CoinDto): CoinDto
 
     /*@GET("/Coinst/{MonedaId}")
     suspend fun getCoin(@Path("MonedaId") MonedaId: String): CoinDto*/
