61c8d328acc337c4efb240896b618e5d3bf9e8ba
vismarlora <vismar_lora@ucne.edu.do>
2022-07-03 22:45:41 -0400

    Implementando la validacion de los campos del registro

--- app/src/main/java/com/example/listadecoinsconapipropiaensomee/ui/ui/screens/CoinRegistroScreen.kt
+++ app/src/main/java/com/example/listadecoinsconapipropiaensomee/ui/ui/screens/CoinRegistroScreen.kt
@@ -1,11 +1,12 @@
 package com.example.listadecoinsconapipropiaensomee.ui.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -30,9 +31,13 @@ fun CoinRegistroScreen(
     val scaffoldState = rememberScaffoldState()
 
     var validar = LocalContext.current
-    val focusRequester = FocusRequester()
-    //val focusRequesterMoneda = FocusRequester()
-    //val focusRequesterPrecio = FocusRequester()
+    val focusRequesterDescripcion = FocusRequester()
+    val focusRequesterPrecio = FocusRequester()
+
+    var error by remember {
+        mutableStateOf(false)
+
+    }
 
     Scaffold(
         topBar = {
@@ -45,7 +50,11 @@ fun CoinRegistroScreen(
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { navHostController.navigate("CoinListScreen") }) {
+                    IconButton(onClick = {
+                        navHostController.navigate(
+                            "CoinListScreen"
+                        )
+                    }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
                             contentDescription = "CONSULTA",
@@ -64,44 +73,154 @@ fun CoinRegistroScreen(
         ) {
 
             OutlinedTextField(
-                value = "",
-                label = { Text(text = "Moneda", fontStyle = FontStyle.Italic) },
-                onValueChange = { },
+                value = viewModel.descripcion,
+                label = {
+                    Text(
+                        text = "Moneda",
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                onValueChange = {
+                    viewModel.descripcion = it
+                    error = false
+                },
+                isError = error,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .focusRequester(focusRequester),
+                    .focusRequester(focusRequesterDescripcion),
                 leadingIcon = {
-                    Icon(imageVector = Icons.Default.CurrencyBitcoin, contentDescription = null)
+                    Icon(
+                        imageVector = Icons.Default.CurrencyBitcoin,
+                        contentDescription = null
+                    )
                 }
             )
 
-            OutlinedTextField(
-                value = "",
-                label = { Text(text = "Precio", fontStyle = FontStyle.Italic) },
-                onValueChange = { },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .focusRequester(focusRequester),
-                leadingIcon = {
-                    Icon(imageVector = Icons.Default.PriceCheck, contentDescription = null)
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            val assistiveElementText = if (error)
+                "Error: Obligatorio" else "*Obligatorio"
+            val assistiveElementColor = if (error) {
+                MaterialTheme.colors.error
+            } else {
+                MaterialTheme.colors
+                    .onSurface
+                    .copy(alpha = ContentAlpha.medium)
+            }
+
+            Text(
+                text = assistiveElementText,
+                color = assistiveElementColor,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(start = 16.dp)
             )
 
             OutlinedTextField(
-                value = "",
-                label = { Text(text = "Url Image", fontStyle = FontStyle.Italic) },
-                onValueChange = { },
+                value = viewModel.valor.toString(),
+                label = {
+                    Text(
+                        text = "Precio",
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                onValueChange = {
+                    viewModel.valor = it.toDouble()
+                    error = false
+                },
+                isError = error,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .focusRequester(focusRequester),
+                    .focusRequester(focusRequesterPrecio),
                 leadingIcon = {
-                    Icon(imageVector = Icons.Default.Image, contentDescription = null)
+                    Icon(
+                        imageVector = Icons.Default.PriceCheck,
+                        contentDescription = null
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            val assistiveText = if (error)
+                "Error: Obligatorio" else "*Obligatorio"
+            val assistiveColor = if (error) {
+                MaterialTheme
+                    .colors
+                    .error
+            } else {
+                MaterialTheme
+                    .colors
+                    .onSurface
+                    .copy(alpha = ContentAlpha.medium)
+            }
+
+            Text(
+                text = assistiveText,
+                color = assistiveColor,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+
+            OutlinedTextField(
+                value = viewModel.imageUrl,
+                label = {
+                    Text(
+                        text = "Url Image",
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                onValueChange = { viewModel.imageUrl = it },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Image,
+                        contentDescription = null
+                    )
                 },
             )
 
-            Spacer(modifier = Modifier.width(20.dp))
-            Button(onClick = { }) {
+            Spacer(
+                modifier = Modifier.width(20.dp)
+            )
+            Button(
+                onClick = {
+                    if (viewModel.descripcion.isNullOrEmpty()) {
+                        error = viewModel.descripcion.isBlank()
+                        Toast.makeText(
+                            validar,
+                            "El campo Moneda está vacio!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        focusRequesterDescripcion.requestFocus()
+                        return@Button
+                    }
+
+                    if (viewModel.valor <= 0) {
+                        error = viewModel.valor.toString().isBlank()
+                        Toast.makeText(
+                            validar,
+                            "El Precio debe ser mayor que cero!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        focusRequesterPrecio.requestFocus()
+                        return@Button
+                    }
+
+
+                    viewModel.Guardar()
+                    viewModel.recargarLista()
+                    navHostController.navigate("CoinListScreen")
+                    Toast.makeText(
+                        validar,
+                        "Se ha Guardado Correctamente!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    viewModel.descripcion = ""
+                    viewModel.imageUrl = ""
+                    viewModel.valor = 0.0
+                    //focusRequester.requestFocus()
+                }
+            ) {
                 Icon(
                     imageVector = Icons.Default.Save,
                     contentDescription = null,
